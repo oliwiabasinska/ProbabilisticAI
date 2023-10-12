66fd00b439fffb756289c8faf9ad3ca92d60b3ed
chbucher <chbucher@ethz.ch>
2023-10-12 20:09:44 +0200

    10k

--- solution.py
+++ solution.py
@@ -129,17 +129,17 @@ class Model(object):
         #    row_ix = np.where(clustered_x == cluster)
         #    print(row_ix)
 
-        subsampled_indices = self.rng.integers(low = 0, high = train_x_2D.shape[0], size = 500)
+        subsampled_indices = self.rng.integers(low = 0, high = train_x_2D.shape[0], size = 10000)
         subsampled_x = train_x_2D[subsampled_indices]
         subsampled_y = train_y[subsampled_indices]
 
 
-        k = 2000  # Number of clusters / new datapoints
-        kmedoids = KMedoids(n_clusters=k)
-        kmedoids.fit(train_x_2D)
+        #k = 2000  # Number of clusters / new datapoints
+        #kmedoids = KMedoids(n_clusters=k)
+        #kmedoids.fit(train_x_2D)
 
-        subsampled_x = train_x_2D[kmedoids.medoid_indices_]
-        subsampled_y = train_y[kmedoids.medoid_indices_]
+        #subsampled_x = train_x_2D[kmedoids.medoid_indices_]
+        #subsampled_y = train_y[kmedoids.medoid_indices_]
 
 
         #plt.violinplot([train_x_2D[:,0],subsampled_x[:,0]])
